6b524b15887fea11678edd6111b8ae2ffd11459b
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2019-07-25 22:33:39 +0530

    Add files via upload

---
+++ 20 prog.py
@@ -0,0 +1,15 @@
+
+i=1
+sum1=0
+count=0
+while(i<=10):
+    #print(i)
+    sum1=sum1+(i*2)
+    #print("after sum")
+    #print(i)
+    i+=1  
+print("sum1 is:",sum1)
+avg=0
+avg=sum1/10
+print("avg is:",avg)
+   
