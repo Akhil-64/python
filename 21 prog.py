9da064eff7cb5cfa416cb26796b9d7f37b88ee11
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2019-07-26 02:24:07 +0530

    Add files via upload

---
+++ 21 prog.py
@@ -0,0 +1,19 @@
+print("pattern 1 is:")
+i=0
+while(i<20):
+    print("*",end="");
+    i+=1
+print("\n")
+print("pattern 2 is:")
+x=1
+while(x<6):
+    y=0
+    while(y<3):
+        print("*",end="")
+        y+=1
+    print("*")
+    x+=1
+    
+    
+
+    
