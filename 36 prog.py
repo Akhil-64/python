4fd13be98f9c0d97f53673cc5b5b501dfbff2843
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2019-07-29 22:54:03 +0530

    Add files via upload

---
+++ 36 prog.py
@@ -0,0 +1,14 @@
+print("enter numbers")
+a=str(input())
+a1=int(a[0:3])
+q=0
+q=2*50*a1//30
+print(int(q**0.5),end=",")
+a2=int(a[4:7])
+q2=0
+q2=2*50*a2//30
+print(int(q2**0.5),end=",")
+a3=int(a[8:11])
+q3=0
+q3=2*50*a3//30
+print(int(q3**0.5))
