f74fea941fb50de14159be6021f7192f6b8e5087
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2019-07-31 00:32:26 +0530

    Add files via upload

---
+++ 33 prog.py
@@ -0,0 +1,19 @@
+print("initial value of investement")
+a=int(input())
+print("annual interest")
+b=int(input())
+print("enter time")
+c=int(input())
+print("value of investment")
+
+total=0
+
+for i in range(1,c+1):
+    
+    sum1= (a*b)/100
+    a=a+sum1
+    
+
+print(a)
+
+    
