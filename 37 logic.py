5ea2ce1fa7fc1dd66e1f2bf10668610ed78d4bc0
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2019-08-04 18:19:39 +0530

    Add files via upload

---
+++ 37 logic.py
@@ -0,0 +1,23 @@
+i2="A"
+for i in range(5):
+    if(i==0):
+      print(i2.ljust(i))
+    if(i==1):
+        print(chr(i+64),chr(i+65))
+    if(i==2):
+        print(chr(i+63),chr(i+64),chr(i+65))
+    if(i==3):
+        print(chr(i+62),chr(i+63),chr(i+64),chr(i+65))
+    if(i==4):
+        print(chr(i+61),chr(i+62),chr(i+63),chr(i+64),chr(i+65))
+#num=65
+for i in range(5):
+    num=65
+    for j in range(0,i+1):
+        print(chr(num),end="")
+        num+=1
+    print("\n")
+        
+    
+        
+    
