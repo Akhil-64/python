d62d94b579b43f5a4ec310a05e702cc3ce76ef5e
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2019-08-06 02:21:11 +0530

    Add files via upload

---
+++ 49 prog.py
@@ -0,0 +1,21 @@
+print("enter string")
+a=str(input())
+b=a.split(" ")
+c=[]
+for i in range(len(b)):
+    c=b[i].capitalize()
+    d=c[0:-1]
+    print(d.capitalize(),end="")
+    e=c[len(c)-1].upper()
+    print(e,end=" ")
+    
+
+
+
+        
+       
+        
+    
+
+
+
