24c3f9ca65fe959e6c591fab16f26ed6e09e9509
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2019-08-10 01:18:23 +0530

    Add files via upload

---
+++ 63 prog.py
@@ -0,0 +1,13 @@
+print("enter size")
+a=int(input())
+print("enter list")
+list1=[]
+for i in range(0,a):
+    x=int(input())
+    list1.append(x)
+print(list1)
+for i in range(0,a):
+    print(list1[i],end="")
+    
+        
+    
