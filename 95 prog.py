b94fb420757e65d3b7b9f7f17b478aa81a405706
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2019-08-27 00:06:54 +0530

    Add files via upload

---
+++ 95 prog.py
@@ -0,0 +1,18 @@
+def pattern(a,b=10,c=15):
+    for i in range(0,a):
+        print("%",end=" ")
+    print()
+    for i in range(0,a):
+        print("a",end=" ")
+    print()
+    for i in range(0,b):
+        print("a",end=" ")
+    print()
+    for i in range(0,c):
+        print("a",end=" ")
+    print()
+    for i in range(0,c):
+        print("a",end=" ")
+    print()
+    
+pattern(7,6)
