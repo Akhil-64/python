eb3cdddc1024c6fea747f0f6bcb258730b3f6dbd
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2019-08-26 20:21:51 +0530

    Add files via upload

---
+++ 90 prog.py
@@ -0,0 +1,40 @@
+print("enter hrs")
+hrs=int(input())
+print("enter min")
+min1=int(input())
+def time(a,b):
+    t=(a*60)+b
+    return(t)
+print(time(hrs,min1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
